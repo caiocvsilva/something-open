87e0fe711d4f8ca290d52e012f702909dcb9ce60
caiocvsilva <caiocvsilva@gmail.com>
2023-04-03 18:17:14 -0400

    starting to choose verbs

--- osar-jsons.py
+++ osar-jsons.py
@@ -33,69 +33,123 @@ def read_jsons():
     return pd.concat(pool, ignore_index=True)
 
 
-# Get all items from pool of jsons in the 'placeholders' index
-def get_placeholders(pool):
+# Get all items from pool of jsons in the 'nouns' index
+def get_nouns(pool):
     return [list(x) for x in set(tuple(x) for x in pool['placeholders'])]
 
 # Get all items from pool of jsons in the 'template' index
-def get_template(pool):
-    return pool['template'].unique()
+def get_verbs(pool):
+    templates = pool['template'].unique()
+    all_verbs = []
 
-# Choose 10% of the placeholders at random, but no two items can share a word
-def choose_placeholders(pool):
+    for sentence in templates:
+
+        tokens = nltk.word_tokenize(sentence)
+
+        # Tag the tokens with their part of speech
+        tagged_tokens = nltk.pos_tag(tokens)
+
+        # Extract the verbs from the tagged tokens
+        verbs = [word for word, pos in tagged_tokens if pos.startswith('V')]
+
+        # append all the verbs
+        all_verbs.append(verbs)
+    return all_verbs
+
+# Choose 10% of the nouns at random, but no two items can share a word
+def choose_nouns(pool):
 
     core_check = True
-    chosen_placeholder=list()
-    chosen_template=list()
+    chosen_noun=list()
     percent_placeholder = 0.0001; 
-    percent_template = 0.1;
-
     
-    placeholders = get_placeholders(pool)
-    templates = get_template(pool)
-    num_placeholders = len(placeholders)
-    num_templates = len(templates)
+    nouns = get_nouns(pool)
+    num_nouns = len(nouns)
 
-    train_placeholders = placeholders
+    train_nouns = nouns
     random_indexes_past=[]
 
-    # list of articles in english language
+    print(num_nouns)
 
-
-
-    print(num_placeholders)
-    print(num_templates)
-    # print("Train Placeholders: ", train_placeholders)
-
-    # generate num_placeholders/10 random numbers in the range 0 to num_placeholders
-    for i in range(int(num_placeholders*percent_placeholder)):
+    # generate num_nouns/10 random numbers in the range 0 to num_nouns
+    for i in range(int(num_nouns*percent_placeholder)):
         core_check = True
         while(core_check):
             core_check=False
             place_dict=[]
-            placeholders_index = random.randint(0, num_placeholders-1)
-            if placeholders_index not in random_indexes_past:
-                random_indexes_past.append(placeholders_index)
+            nouns_index = random.randint(0, num_nouns-1)
+            if nouns_index not in random_indexes_past:
+                random_indexes_past.append(nouns_index)
+                chosen_noun.append(nouns[nouns_index])
             else:
                 core_check = True
                 continue
-            for j, val in enumerate(placeholders[placeholders_index]):
-                train_placeholders.remove(placeholders[placeholders_index])
+            print('len of nouns: ', len(nouns))
+            print('nouns_index: ', nouns_index)
+            for j, val in enumerate(nouns[nouns_index]):
+                train_nouns.remove(nouns[nouns_index])
+                num_nouns = len(train_nouns)
 
                 core_check = False
                 # Extracting all nouns from first_list
                 nouns_to_check = [word[0] for word in nltk.pos_tag([word for sublist in val for word in sublist]) if word[1].startswith('N')]
-                if not any(word in sublist for sublist in train_placeholders for word in nouns_to_check):
+                if not any(word in sublist for sublist in train_nouns for word in nouns_to_check):
                     place_dict.append(val)
                 else:
                     core_check = True
                     break
 
-    for i in range(int(num_placeholders*percent_placeholder)):
-        chosen_placeholder.append(placeholders[random_indexes_past[i]])
+    print("nouns chosen: ", chosen_noun)
 
-    print("Placeholders chosen: ", chosen_placeholder)
+# Choose 10% of the verbs at random, similar to what was done for nouns
+def choose_verbs(pool):
+    
+    core_check = True
+    chosen_verbs=list()
+    percent_verbs = 0.1; 
+    
+    verbs = get_verbs(pool)
+    num_verbs = len(verbs)
+
+    train_verbs = verbs
+    random_indexes_past=[]
+
+    print(num_verbs)
+
+    # generate num_verbs/10 random numbers in the range 0 to num_verbs
+    for i in range(int(num_verbs*percent_verbs)):
+        core_check = True
+        while(core_check):
+            core_check=False
+            verbs_index = random.randint(0, num_verbs-1)
+            if verbs_index not in random_indexes_past:
+                random_indexes_past.append(verbs_index)
+                chosen_verbs.append(verbs[verbs_index])
+            else:
+                core_check = True
+                continue
+            print('len of verbs: ', len(verbs))
+            print('verbs_index: ', verbs_index)
+            for j, val in enumerate(verbs[verbs_index]):
+                train_verbs.remove(verbs[verbs_index])
+                num_verbs = len(train_verbs)
+
+                core_check = False
+                # Extracting all verbs from first_list
+                verbs_to_check = [word[0] for word in nltk.pos_tag([word for sublist in val for word in sublist]) if word[1].startswith('V')]
+                if not any(word in sublist for sublist in train_verbs for word in verbs_to_check):
+                    chosen_verbs.append(val)
+                else:
+                    core_check = True
+                    break
+
+    # for i in range(int(num_verbs*percent_verbs)):
+    #     chosen_verbs.append(verbs[random_indexes_past[i]])
+
+    print("verbs chosen: ", chosen_verbs)
+
 
 if __name__ == "__main__":
     pool = read_jsons()
-    choose_placeholders(pool)
+    choose_nouns(pool)
+    choose_verbs(pool)
