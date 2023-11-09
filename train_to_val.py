488fe3ef1d42b587b05a731e98cd388ee4d25882
caiocvsilva <caiocvsilva@gmail.com>
2023-11-09 14:12:14 -0500

    fixing validation csv

---
+++ train_to_val.py
@@ -0,0 +1,39 @@
+
+import pandas as pd
+import sys
+
+path_file = sys.argv[1]
+
+# Read the txt file into a list of strings
+with open(path_file, 'r') as f:
+    lines = f.readlines()
+
+# Split each line into path and label
+data = [line.strip().split() for line in lines]
+
+# Create a DataFrame from the data
+df = pd.DataFrame(data, columns=['path', 'label'])
+
+# Get unique labels and their counts
+unique_labels = df['label'].value_counts()
+
+# Get labels with count higher than 10
+high_count_labels = unique_labels[unique_labels > 10].index
+
+# Split the dataframe into train and val
+train = df[~df['label'].isin(high_count_labels)]
+val = df[df['label'].isin(high_count_labels)].sample(frac=0.2)
+
+print(train.shape)
+print(val.shape)
+
+
+train_output_path = path_file.split('.')[0]+'v2.txt'
+val_output_path = path_file.split('.')[0].replace('train', 'val')+'v2.txt'
+
+# Save the train and val dataframes to csv
+train.to_csv(train_output_path, index=False)
+val.to_csv(val_output_path, index=False)
+
+
+
