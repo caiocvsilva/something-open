213c65385c04f1e9b591bba521d1800e15ba9c8a
caiocvsilva <caiocvsilva@gmail.com>
2024-02-16 13:26:12 -0500

    Refactor train_acc.py to calculate and store top1 and top5 accuracy during training

--- train_acc.py
+++ train_acc.py
@@ -11,34 +11,31 @@ arr_top1 = []
 arr_top5 = []
 
 with open(log_file_path, "r") as file:
-    start_reading = False
     for line in file:
-        if "mmengine - INFO - Checkpoints will be saved to" in line:
-            start_reading = True
-            continue
-        if start_reading:
-            if "Epoch(train)" in line:
-                if iteration > 0:
-                    top1_all_train /= iteration
-                    top5_all_train /= iteration
-                    print(f"Iteration {epoch}: top1_acc_train = {top1_all_train}, top5_acc_train = {top5_all_train}")
-                    arr_top1.append(top1_all_train)
-                    arr_top5.append(top5_all_train)
+        top1_matches = re.findall(r'top1_acc_train: (\d+\.\d+)', line)
+        top5_matches = re.findall(r'top5_acc_train: (\d+\.\d+)', line)
+        
+        for match in top1_matches:
+            top1_acc_train = float(match)
+            top1_all_train += top1_acc_train
+            iteration += 1
 
-                    top1_all_train = 0.0
-                    top5_all_train = 0.0
-                    iteration = 0
-                    epoch += 1
-                continue
-            entries = re.split('top1_acc_train:|top5_acc_train:', line)
-            for entry in entries[1:]:
-                if 'top1_acc_train' in entry:
-                    top1_acc_train = float(entry.strip())
-                    top1_all_train += top1_acc_train
-                    iteration += 1
-                elif 'top5_acc_train' in entry:
-                    top5_acc_train = float(entry.strip())
-                    top5_all_train += top5_acc_train
+        for match in top5_matches:
+            top5_acc_train = float(match)
+            top5_all_train += top5_acc_train
+
+        if "Epoch(train)" in line:
+            if iteration > 0:
+                top1_all_train /= iteration
+                top5_all_train /= iteration
+                print(f"Iteration {epoch}: top1_acc_train = {top1_all_train}, top5_acc_train = {top5_all_train}")
+                arr_top1.append(top1_acc_train)
+                arr_top5.append(top5_acc_train)
+
+                top1_all_train = 0.0
+                top5_all_train = 0.0
+                iteration = 0
+                epoch += 1
 
 with open("arr_top1.pkl", "wb") as file:
     pickle.dump(arr_top1, file)
