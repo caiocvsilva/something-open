f9c02689c74ac0106fecd0d1b8e78f917dae281d
caiocvsilva <caiocvsilva@gmail.com>
2024-02-16 13:11:50 -0500

    Fix calculation of top1_acc_train in train_acc.py

--- train_acc.py
+++ train_acc.py
@@ -34,7 +34,14 @@ with open(log_file_path, "r") as file:
                 top1_all_train += top1_acc_train
                 iteration += 1
             elif "top5_acc_train" in line:
-                top5_acc_train = float(line.split(":")[1].strip())
+                if "top1_acc_train" in line:
+                    line1, line2 = line.split("top1_acc_train")
+                    top1_acc_train = float(line2.split(":")[1].strip())
+                    top1_all_train += top1_acc_train
+                    iteration += 1
+                else:
+                    line1 = line
+                top5_acc_train = float(line1.split(":")[1].strip())
                 top5_all_train += top5_acc_train
 
 with open("arr_top1.pkl", "wb") as file:
