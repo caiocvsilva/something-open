d18ef965a73b4e5deb29e986064af941b880f0f4
caiocvsilva <caiocvsilva@gmail.com>
2024-02-21 15:31:48 -0500

    Update plot_loss.py to include validation accuracy and save accuracy plot

--- plot_loss.py
+++ plot_loss.py
@@ -2,11 +2,14 @@ import os
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
+import pickle
 
 def parse_log_file(log_file):
-    data = {'iteration': [], 'train_loss': [], 'val_loss': []}
+    data = {'iteration': [], 'train_loss': [], 'val_loss': [], 'val_acc_top1': [], 'val_acc_top5': []}
     iteration = 0
-    val_loss = 2.3053  # Initialize val_loss variable
+    val_loss = 2.2990 # Initialize val_loss variable
+    val_acc_top1 = 0.1231
+    val_acc_top5 = 0.4974
     with open(log_file, 'r') as file:
         for line in file:
             if 'Epoch(train)' in line and 'loss:' in line:
@@ -18,21 +21,22 @@ def parse_log_file(log_file):
                 data['train_loss'].append(loss)
                 if val_loss is not None:
                     data['val_loss'].append(val_loss)
+                    data['val_acc_top1'].append(val_acc_top1)
+                    data['val_acc_top5'].append(val_acc_top5)
             if 'Epoch(val)' in line and 'acc/loss_cls:' in line:
                 parts = line.split()
                 epoch = int(parts[parts.index('Epoch(val)') + 1].split('[')[1].split(']')[0])
-                loss = float(parts[parts.index('acc/loss_cls:') + 1])
-                val_loss = loss
+                val_loss = float(parts[parts.index('acc/loss_cls:') + 1])
+                val_acc_top1 = float(parts[parts.index('acc/top1:') + 1])
+                val_acc_top5 = float(parts[parts.index('acc/top5:') + 1])
     return pd.DataFrame(data)
 
-# def plot_loss(df):
-#     plt.figure(figsize=(10, 6))
-#     plt.plot(df['iteration'], df['loss'], label='Loss')
-#     plt.xlabel('Iteration')
-#     plt.ylabel('Loss')
-#     plt.title('Loss over iterations')
-#     plt.legend()
-#     plt.savefig('loss_over_epochs_knuv.png')  # saves the plot to a file 
+def load_pickle(file_path):
+    with open(file_path, 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+
 
 def plot_loss(df):
     plt.figure(figsize=(10, 6))
@@ -40,16 +44,35 @@ def plot_loss(df):
     df['loss_smooth'] = df['train_loss'].rolling(window=10).mean()
     # Plot the smooth train loss and the val loss
     plt.plot(df['iteration'], df['loss_smooth'], label='Train loss')
-    plt.plot(df['iteration'], df['val_loss'], label='Validation loss')
-    # plt.plot(df['iteration'], df['loss_smooth'], label='Loss')
+    plt.plot(df['iteration'], df['val_loss'], label='Validation loss', color='red')
     plt.xlabel('Iteration')
     plt.ylabel('Loss')
     plt.title('Loss over iterations')
     plt.legend()
     plt.savefig('loss_over_epochs_unkv_val.png')  # saves the plot to a file
 
-# log_file = '20231116_173410.log'  # replace with your log file path
+    window_size = 20  # Define the size of the window
+    if 'train_acc_top1' in df.columns and 'train_acc_top5' in df.columns:
+        plt.figure(figsize=(10, 6))
+        plt.plot(df['iteration'], df['train_acc_top1'].rolling(window_size).mean(), label='Train accuracy top1', color='blue', zorder=3)
+        plt.plot(df['iteration'], df['val_acc_top1'].rolling(window_size).mean(), label='Validation accuracy top1', color='red', zorder=2)
+        plt.plot(df['iteration'], df['train_acc_top5'].rolling(window_size).mean(), label='Train accuracy top5', color='darkblue', alpha=0.8, zorder=1)
+        plt.plot(df['iteration'], df['val_acc_top5'].rolling(window_size).mean(), label='Validation accuracy top5', color='darkred', alpha=0.8, zorder=0)
+        plt.xlabel('Iteration')
+        plt.ylabel('Accuracy')
+        plt.title('Accuracy over iterations')
+        plt.legend()
+        plt.savefig('accuracy_over_epochs_unkv_val.png')
+
 log_file = sys.argv[1]
 df = parse_log_file(log_file)
-print(df)
+if len(sys.argv) > 2:
+    train_acc_top1 = load_pickle(sys.argv[2])
+    train_acc_top5 = load_pickle(sys.argv[3])
+
+    df['train_acc_top1'] = train_acc_top1
+    df['train_acc_top5'] = train_acc_top5
+
+
+# print(df)
 plot_loss(df)
--- train_acc.py
+++ train_acc.py
@@ -5,7 +5,8 @@ import re
 log_file_path = sys.argv[1]
 top1_all_train = 0.0
 top5_all_train = 0.0
-iteration = 0
+iteration_top1 = 0
+iteration_top5 = 0
 epoch = 0
 arr_top1 = []
 arr_top5 = []
@@ -18,24 +19,29 @@ with open(log_file_path, "r") as file:
         for match in top1_matches:
             top1_acc_train = float(match)
             top1_all_train += top1_acc_train
-            iteration += 1
+            iteration_top1 += 1
 
         for match in top5_matches:
             top5_acc_train = float(match)
             top5_all_train += top5_acc_train
+            iteration_top5 += 1
 
         if "Epoch(train)" in line:
-            if iteration > 0:
-                top1_all_train /= iteration
-                top5_all_train /= iteration
+            if iteration_top1 > 0:
+                top1_all_train /= iteration_top1
+                top5_all_train /= iteration_top5
                 print(f"Epoch {epoch}: top1_acc_train = {top1_all_train}, top5_acc_train = {top5_all_train}")
-                arr_top1.append(top1_acc_train)
-                arr_top5.append(top5_acc_train)
+                arr_top1.append(top1_all_train)  # append average accuracy
+                arr_top5.append(top5_all_train)  # append average accuracy
 
                 top1_all_train = 0.0
                 top5_all_train = 0.0
-                iteration = 0
+                iteration_top1 = 0
+                iteration_top5 = 0
                 epoch += 1
+
+# print last 5 elements of the list
+print(arr_top1[-5:])
 
 with open("arr_top1.pkl", "wb") as file:
     pickle.dump(arr_top1, file)
